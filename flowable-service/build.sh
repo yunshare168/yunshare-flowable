1d840547a26800cacdc286e8d7e34721d3738e5d
liuzhixian <lzx@yuyuda.com>
2024-08-03 13:55:20 +0800

    调整Dockerfile

--- flowable-service/build.sh
+++ flowable-service/build.sh
@@ -1,5 +1,5 @@
 mvn clean package
 
-docker build -t registry.cn-hangzhou.aliyuncs.com/yunshare/yunshare-bpm-service:1.0.0 .
+docker build -t registry.cn-hangzhou.aliyuncs.com/yunshare/flowable-service:1.0.0 .
 
-docker push registry.cn-hangzhou.aliyuncs.com/yunshare/yunshare-bpm-service:1.0.0
+docker push registry.cn-hangzhou.aliyuncs.com/yunshare/flowable-service:1.0.0
